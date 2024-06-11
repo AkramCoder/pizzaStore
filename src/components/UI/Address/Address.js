2bd0654ecf2f83646942f0e78b01cb82120b740e
Mohamed Akram <m.akeblersane@esi-sba.dz>
2024-06-11 13:54:31 +0100

    Pizza order store

---
+++ src/components/UI/Address/Address.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Address(props) {
+  const { buildingNumber, city, state, country, pinCode, streetName } = props;
+  return (
+    <div>
+      <strong>Address:</strong>
+      <br />
+      {buildingNumber ? (
+        <p className="mb-0">
+          {buildingNumber} {streetName}
+        </p>
+      ) : null}
+      {city ? (
+        <p className="mb-0">
+          {city}, {state}, {country}
+        </p>
+      ) : null}
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/store/reducers/cartReducer.js
@@ -0,0 +1,63 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  cart: [],
+  itemMap: {},
+  totalPrice: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  let updatedCart = [...state.cart];
+  let updatedPrice = state.totalPrice;
+  const updatedMap = { ...state.itemMap };
+
+  switch (type) {
+    case actionTypes.CLEAR_CART:
+      return initialState;
+
+    case actionTypes.ADD_ITEM_TO_CART:
+      if (payload.item.id in state.itemMap) {
+        updatedCart[state.itemMap[payload.item.id]].quantity += 1;
+      } else {
+        updatedMap[payload.item.id] = updatedCart.length;
+        updatedCart.push({
+          ...payload.item,
+          quantity: 1,
+        });
+      }
+      updatedPrice += payload.item.price;
+      return {
+        ...state,
+        cart: updatedCart,
+        itemMap: updatedMap,
+        totalPrice: updatedPrice,
+      };
+
+    case actionTypes.REMOVE_ITEM_FROM_CART:
+      if (!(payload.item.id in state.itemMap)) {
+        return state;
+      }
+      updatedPrice -= payload.item.price;
+      updatedCart[state.itemMap[payload.item.id]].quantity -= 1;
+      if (updatedCart[state.itemMap[payload.item.id]].quantity === 0) {
+        updatedCart = updatedCart.filter((item) => item.id !== payload.item.id);
+        const index = updatedMap[payload.item.id];
+        delete updatedMap[payload.item.id];
+        for (let i = index; i < updatedCart.length; i++) {
+          updatedMap[updatedCart[i].id] -= 1;
+        }
+      }
+      return {
+        ...state,
+        cart: updatedCart,
+        itemMap: updatedMap,
+        totalPrice: updatedPrice,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
